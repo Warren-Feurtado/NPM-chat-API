d212bbe0003c052ef822e2d38845da48477012a9
Warren Feurtado <wolfefeurtado@gmail.com>
2023-05-25 14:41:00 -0500

    mssgs being sent to server and displayed in window

--- src/components/chat-component/chat.component.functions.js
+++ src/components/chat-component/chat.component.functions.js
@@ -1,12 +1,18 @@
-import { io } from "socket.io-client";
+// import { io } from "socket.io-client";
 import { SOCKETAPI } from "../../environment/env";
+import { SocketService } from "../../utils/socket.service"
 
 export class ChatFunctions {
-  #socket = io(SOCKETAPI); 
-  #snmsp;
+  // #socket = io(SOCKETAPI); 
+  #socketService;
+  // #socket;
+  // #nmSpace;
+  convoID;
   
-  // domain = window.location.hostname;
+  // domain = {domain: window.location.hostname};
   domain = {domain: 'test.com'};
+  compNID;
+  comNSP;
    newMsgHistory = [];  
    oldMsgHistory = [];
    newInMsg = '';
@@ -25,125 +31,62 @@ export class ChatFunctions {
      * 
      */
     constructor(shadowRoot) {
-      // this.services = services;
-      // this.newMsgObj = this.services.socketService.msgObj;
-      // console.log("new msgObj in component constructor: ", this.newMsgObj);
+      this.#socketService = new SocketService;
       this.chatToggle = this.chatToggle(shadowRoot);
       this.sendMsg = this.sendMsg(shadowRoot);
+      // this.msgDisplay = this.msgDisplay(shadowRoot);
+      this.myData = this.myData(shadowRoot);
       this.newMsgIn = this.newMsgIn(shadowRoot);
-      this.chatOpen = this.chatOpen(shadowRoot);
+      // this.chatOpen = this.chatOpen(shadowRoot);
       // this.newMsgOut = this.newMsgOut(this.msg); 
       
       this.myData();
       this.newMsgIn();
+      this.#socketService.getNspId(shadowRoot);
         
-       
     }
+    myData = (shadowRoot) => () => {
+      let aclUD = {token: localStorage.getItem('aclUD')};
 
-    myData() {
-      let aclUD = localStorage.getItem('aclUD');
+      // console.log('checking if values are assigned from constructor: ', (this.#socketService));
       
+      this.#socketService.visCon(this.domain);  
+      this.#socketService.nspCon(aclUD);
 
-      this.#socket.emit('client:connect', this.domain);
-      console.log("here's the domain im connecting from", this.domain);
-
-      this.#socket.on('nsps_id', (data) => {
-        this.#snmsp = io(`${SOCKETAPI}/${data.nsps_id}`);
-        console.log('nsps_id object from server: ', data);
-        console.log('new name space in function: ', this.#snmsp);
-      })
-      console.log('new name space outside function: ', this.#snmsp);
-
-      
-
-      this.#socket.emit('aclUD', aclUD);
-      
-      this.#socket.on('myData', (myData) => {
-        console.log('here is my metadata from server: ', myData);
-        if(aclUD === null) {
-          console.log('NO aclUD found: ', aclUD);
-          this.metaData = myData;
-          console.log('local storage was empty... setting new metadata: ' , this.metaData); 
-          localStorage.setItem('aclUD', this.metaData.uuID);
-          console.log('new aclUD set in Local storage: ', localStorage.getItem('aclUD'));
-
-          // this.#socket.on('new-convo', (convoData) => {
-          //   console.log('new convo data created', convoData);
-          // })
-          
-        } else {
-          this.metaData = myData;
-          console.log('aclUD already exists: ', aclUD); 
-          console.log('metadata with existing aclUD', this.metaData); 
-
-          // this.#socket.on('convo-history', (convoData) => {
-          //   console.log('user convo found', convoData);
-          // })
-        }
-
-        console.log('current metadata after processing', this.metaData);
-
-      })
-    }
-
-    chatOpen = (shadowRoot) => () => {
-      let sender = localStorage.getItem('aclUD');
-      // convoData.seenBy.push(localStorage.getItem('aclUD'));
-      console.log('Chat window opened by: ', sender);
-
-      this.#socket.emit('chatOpen', sender);
-
-      this.#socket.on('chat-history', (history) => {
-        let msgHistory = shadowRoot.querySelector("#msg_list");
-
-        this.oldMsgHistory = history;
-        console.log('oldMsgHistory', this.oldMsgHistory);
-
-        this.oldMsgHistory.forEach((msg) => {
-          let listItem = document.createElement('p');
-          listItem.textContent = msg.message;
-          msgHistory.appendChild(listItem);
-        })
+      this.#socketService.on('visitor:reconnect', (reconData) => {
+        console.log('reconnect data from Server', reconData);
       })
 
-      
+      this.#socketService.on('visitor:saved', (convoID) => {
+        console.log('new convo started...', convoID);
+        this.convoID = convoID;
+      })
+
+      this.#socketService.on('visitor:namespace_id', (nspID) => {
+        console.log('got nspID in component', nspID);
+        this.compNID = nspID.namespace_id;
+        this.comNSP = `${SOCKETAPI}/${this.compNID}`;
+        console.log('company namespace in component: ', this.comNSP);
+      })
+
+      this.#socketService.msgIn(this.comNSP, (msgData) => {
+        console.log('new message from server', msgData);
+      })
     }
 
 
     newMsgIn = (shadowRoot) => () => {
-    
-      this.#socket.on('new-msg', (data) => {
-        let msgHistory = shadowRoot.querySelector("#msg_list");
-
-
-        console.log("new message in component: ", data);
-        this.newMsgHistory.push(data.message.message);
-        console.log('heres the history', this.newMsgHistory);
-        let listItem = document.createElement('p');
-        listItem.textContent = data.message.message;
-        msgHistory.appendChild(listItem);
-
-
-        // this.newMsgHistory.forEach(msgObject => {
-        //       let listItem = document.createElement('p')
-        //       listItem.textContent = msgObject.message;
-        //   //     console.log('heres the msg object:', msgObject);
-        //   //     console.log('message object.message', msgObject.message);
-        //   //     listItem.textContent = msgObject.message;
-        //   mgsHistory.appendChild(listItem);
-        // });
-        
-      });
+      
+      // this.#socketService.newMsgIn((msgObj) => {
+      //   console.log('new message object received in component: ', msgObj);
+      //   let msgHistory = shadowRoot.querySelector('#msg_list');
+      //   let listItem = document.createElement('p');
+      //   this.newMsgHistory.push(msgObj.message);
+      //   console.log('heres the history', this.newMsgHistory);
+      //   listItem.textContent = msgObj.message;
+      //   msgHistory.appendChild(listItem);
+      // });
     };
-
-    newMsgOut(msg) {
-      let msgData = {
-        message: msg,
-        // chatID: ''
-      }
-      console.log("message heading for server from component", msgData);
-      this.#socket.emit('new-msg', msgData);
-    }
 
   
     // TOGGLE CHAT WINDOW OPEN AND CLOSE
@@ -177,33 +120,35 @@ export class ChatFunctions {
       }
     };
 
-      sendMsg = (shadowRoot) => (event) => {
-        event.preventDefault();
+    sendMsg = (shadowRoot) => (event) => {
+      event.preventDefault();
         
-        this.msg = shadowRoot.querySelector("#newMsg");
+      this.msg = shadowRoot.querySelector("#newMsg");
+        
+      // MESSAGE FIELD VALIDATION
+      if(this.msg.value == '') {
+        return alert('Please type a message to send!!');
+          
+      } else {
+        let msgOutDisplay = shadowRoot.querySelector("#msg_list");
+        console.log('company namespace wen sending messsage: ', this.comNSP)
+        this.#socketService.newMsgOut(this.msg.value, this.convoID, this.comNSP);
+        console.log("This is the message being sent: ", this.msg.value);
 
-        // this.mgsHistory = shadowRoot.querySelector("#msg_list");
+        let msgEl = document.createElement('p');
+        msgEl.textContent = this.msg.value;
+        msgOutDisplay.appendChild(msgEl);
 
-        // let msg_List = shadowRoot.querySelector("#msg_list")
-
-        // MESSAGE FIELD VALIDATION
-        if(this.msg.value == '') {
-          return alert('Please type a message to send!!');
-
-        } else {
-          this.newMsgOut(this.msg.value);
-          console.log("This is the message being sent: ", this.msg.value);
-
-        }
-
-        this.msg.value = '';
-      };
-
-      newUser() {
-        let aclUD = localStorage.getItem('aclUD');
-        console.log("UD before removal",  aclUD)
-        localStorage.removeItem('aclUD');
-        console.log("removed UD", aclUD);
       }
+      this.msg.value = '';
+    };
+
+    newUser() {
+      let aclUD = localStorage.getItem('aclUD');
+      console.log("UD before removal",  aclUD)
+      localStorage.removeItem('aclUD');
+      console.log("removed UD", aclUD);
+    }
+    
 }
   
--- src/utils/socket.service.js
+++ src/utils/socket.service.js
@@ -1,20 +1,119 @@
-// import { io } from "socket.io-client";
-// import { SOCKETAPI } from "../environment/env";
+import { io } from "socket.io-client";
+import { SOCKETAPI } from "../environment/env";
 
-// export class SocketService {
-//     socket = io(SOCKETAPI); 
-//     mgsList = [];
-//     newInMsg = '';
-//     msgObj;
-//     // msgObj = {message: String, history: Array};
-//     // msgObj = {message, history};
-//     msgData;
+export class SocketService {
+    // socket = io;
+    socket = io(SOCKETAPI);
+    // compNID;
+    compNSP;
+    mgsHistory = [];
 
 
-//     constructor() {
-//         // this.newMsgIn();
+    constructor() {
+        // this.newMsgIn();
+        // this.getNspId();
+        this.newToken();
+        this.agentConn();
         
-//     }
+    }
+
+    on(evName, cb) {
+        this.socket.on(evName, (data) => {
+            cb(data);
+        })
+    }
+    
+    // nspOn(evName, cb) {
+    //     this.compNSP.on(evName, (data) => {
+    //         cb(data);
+    //     })
+    // }
+    
+    // nspOn(evName, cb) {
+    //     this.compNID.on(evName, (data) => {
+    //         cb(data);
+    //     })
+    // }
+
+    getNspId() {
+        this.on('visitor:namespace_id', (nspID) => {
+            let compNID = nspID.namespace_id;
+            this.compNSP = io(`${SOCKETAPI}/${compNID}`);
+            // this.compNSP.emit('visitor:message', testMsgObj);
+            
+            this.compNSP.on('visitor:new_message', (msgObj) => {
+                console.log('message from my namespace received in service', msgObj);
+
+                // let msgHistory = shadowRoot.querySelector('#msg_list');
+                // let listItem = document.createElement('p');
+                // this.newMsgHistory.push(msgObj.message);
+                // console.log('heres the history', this.newMsgHistory);
+                // listItem.textContent = msgObj.message;
+                // msgHistory.appendChild(listItem);
+            });
+
+            // console.log("nspID object from server in service: ", nspID);
+            // console.log("compNSP object from server in service: ", this.compNSP);
+        });
+        
+    }
+
+    newToken() {
+        this.on('visitor:token', (token) => {
+            console.log('new token object from server in service: ', token);
+            localStorage.setItem('aclUD', token.clientToken);
+        })
+    }
+
+    agentConn() {
+        this.on('visitor:agent_connected', (data) => {
+            console.log('agent connect data: ', data);
+        })
+    }
+
+    // newMsgIn(cb) {
+    //     this.on('visitor:new_message', (msgObj) => {
+    //         cb(msgObj);
+    //     })
+    // }
+
+    //"VISITOR CONNECT" EMITTER
+    visCon(domain) {
+        this.socket.emit("visitor:connect", domain);
+        console.log('Domain sent...: ', domain);
+    }
+
+    //"NAME SPACE CONNECT" EMITTER
+    nspCon(aclUD) {
+        this.socket.emit('visitor:namespace_connect', aclUD);
+        console.log('aclUD sent...: ', aclUD);
+    }
+
+    //MESSAGE OUT EMITTER
+    newMsgOut(msg, convoID, compNSP) {
+        let msgObj = {
+            message: msg,
+            token: localStorage.getItem('aclUD'),
+            conversationID: convoID
+            // namespace: this.compNSP,
+        }
+
+        io(compNSP).emit('visitor:message', msgObj);
+
+        // this.compNSP.emit('visitor:message', msgObj);
+        console.log('new message object out: ', msgObj);
+    }
+
+    msgIn(compNSP, cb) {
+        io(compNSP).on('visitor:new_message', (msgObj) => {
+            cb(msgObj);
+        })
+    }
+
+    
+
+    
+}
 
 //     // newMsgIn(mgsList) { 
 //     //     this.#socket.on('new-msg', (data) => {
